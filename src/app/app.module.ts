007315cd19be7ee1952cac2abbd5ffca62fdb449
Nikhil Devgire <nikhil.devgire@agiliad.com>
2020-01-07 16:40:13 +0530

    Recipe model added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
-    RecipesComponent
+    RecipesComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://p0.pxfuel.com/preview/431/473/191/noodle-dish-chicken-farmer-cook.jpg'),
+    new Recipe('A test recipe', 'This is simply a test', 'https://p0.pxfuel.com/preview/431/473/191/noodle-dish-chicken-farmer-cook.jpg'),
+    new Recipe('A test recipe', 'This is simply a test', 'https://p0.pxfuel.com/preview/431/473/191/noodle-dish-chicken-farmer-cook.jpg'),
+    new Recipe('A test recipe', 'This is simply a test', 'https://p0.pxfuel.com/preview/431/473/191/noodle-dish-chicken-farmer-cook.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
